380233444df078855bb81e86d55c6e28101ef00a
Liridona <91525128+barlaek@users.noreply.github.com>
2022-01-30 12:35:45 +0100

    js Q1/2/3

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// const cats = [
+//     {
+//         name: "Blob",
+//         age: 10
+//     },
+//     {
+//         name: "Harold",
+//     },
+//     {
+//         name: "Blurt",
+//         age: 21
+//     }
+// ];
+
+// Question 1
+
+const cat = {
+    complain: function() {
+        console.log("Meow!");
+    }
+}
+
+cat.complain();
+
+// Logs "Meow!"
+
+// Question 2
+
+const heading = document.querySelector("h3");
+heading.innerHTML = `<h3 styles="font: 2em">Updated Heading</h3>`;
+
+// Question 3
+
