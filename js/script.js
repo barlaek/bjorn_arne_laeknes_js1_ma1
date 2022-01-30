092e3648483d1c1d2367f4d0d58f03ceab0fb092
Liridona <91525128+barlaek@users.noreply.github.com>
2022-01-30 14:43:24 +0100

    Q5/6/7/8(start)

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-// const cats = [
-//     {
-//         name: "Blob",
-//         age: 10
-//     },
-//     {
-//         name: "Harold",
-//     },
-//     {
-//         name: "Blurt",
-//         age: 21
-//     }
-// ];
 
 // Question 1
 
@@ -36,3 +23,53 @@ document.querySelector("h3").style.fontSize = "2em";
 // Question 4
 
 heading.className += "subheading";
+
+// Question 5
+
+const paragraph = document.querySelectorAll("p");
+
+for(let i = 0; i < paragraph.length; i++) {
+    paragraph[i].style.color = "red";
+}
+
+// Question 6
+
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML = `<p>New Paragraph</p>`;
+
+resultsContainer.style.background = "yellow";
+
+// Question 7
+
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+function listLoop(list) {
+    for(let i = 0; i < list.length; i++) {
+        console.log(cats[i].name);
+    }
+}
+
+listLoop(cats);
+
+// Logs Blog, Harold, and Blurt
+
+// Question 8
+
+function createCats(cats) {
+    for(let i = 0; i < cats.length; i++) {
+        
+    }
+}
